af9f995f5d87f34bb0c52d5b73356e7d566b0818
emmahughescodes <emma.setohughes@gmail.com>
2018-12-16 13:17:59 -0500

    added smooth scrolling and inital plans for  falling leaf animation :lipstick:

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 3000);
+    }
+});
